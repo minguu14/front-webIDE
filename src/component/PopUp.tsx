189ad0e6a52affae430ad60d00786f576c9a5162
heekwang <ekzm7040@naver.com>
2024-03-08 12:23:49 +0900

    회원가입 파업창

--- src/component/PopUp.tsx
+++ src/component/PopUp.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import InputBox from "./InputBox";
+import { Link } from "react-router-dom";
 export default function PopUp() {
   return (
-    <div className="">
-      <div>회원가입이 완료되었습니다</div>
+    <div className="w-srceen h-screen flex justify-center items-center">
+      <div className="w-96 h-48 bg-gray-50  border rounded flex flex-col justify-center items-center">
+        <div className="flex justify-center items-center font-semibold mb-10">
+          <div className="text-sky-500">회원가입</div>이 완료되었습니다
+        </div>
+        <Link to={"/"}>
+          <button className="w-80 h-11 rounded bg-blue-950 text-white">
+            {" "}
+            완료
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
